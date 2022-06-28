97850ca4aa6e814741b5b22065e6ad3f556c494d
crow <abelx85c@gmail.com>
2022-06-28 15:30:18 +0800

    刪除target

---
+++ src/main/java/com/store/interceptor/LoginInterceptor.java
@@ -0,0 +1,14 @@
+package com.store.interceptor;
+
+/**
+ * @檔案名稱: LoginInterceptor
+ * @創建者:
+ * @創建日期: 2022-06-28 15:29
+ * @編輯器: IntelliJ IDEA
+ * @用途:
+ * @參數:
+ * @回傳值:
+ */
+public class LoginInterceptor {
+
+}
