6eb5ee728e74eda14c808954de18f3e9d433ecac
crow <abelx85c@gmail.com>
2022-06-29 15:38:42 +0800

    使用者修改密碼

--- src/main/java/com/store/mapper/UserMapper.java
+++ src/main/java/com/store/mapper/UserMapper.java
@@ -2,10 +2,23 @@ package com.store.mapper;
 
 import com.store.entity.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Date;
 
 @Mapper
 public interface UserMapper {
+
   Integer insert(User user);
 
   User findByUsername(String username);
+
+  Integer updatePasswordByUid(
+          @Param("uid") Integer uid,
+          @Param("password") String password,
+          @Param("modifiedUser") String modifiedUser,
+          @Param("modifiedTime") Date modifiedTime
+  );
+
+  User findByUid(Integer uid);
 }
--- src/test/java/com/store/mapper/UserMapperTest.java
+++ src/test/java/com/store/mapper/UserMapperTest.java
@@ -1,48 +1,56 @@
-//package com.crow.cart.mapper;
-//
-//import com.store.entity.User;
-//import com.store.mapper.UserMapper;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import java.text.SimpleDateFormat;
-//import java.util.Date;
-//import org.springframework.util.DigestUtils;
-//
-//
-//@SpringBootTest
+package com.store.mapper;
+
+import com.store.entity.User;
+import com.store.mapper.UserMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.springframework.util.DigestUtils;
+
+
+@SpringBootTest
 //@RunWith(SpringRunner.class)
-//public class UserMapperTest {
-//	@Autowired
-//	private UserMapper userMapper;
-//
-//	@Test
-//    public void insert() {
-//		System.out.println("================");
-//		System.out.println("測試使用者新增");
-//
-//		Date date = new Date();
-//    	String nowTimeString = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date);
-//
-//    	String md5String = DigestUtils.md5DigestAsHex(nowTimeString.getBytes());
-//		String testString = "test_" + md5String.substring(0, 3);;
-//		System.out.println("testString: "+ testString);
-//
-//    	User user = new User();
-//    	user.setUserAccount(testString);
-//    	user.setUserName(testString);
-//    	user.setPassword("123");
-//    	user.setEmail(testString + "@gmail.com");
-//    	user.setPhone("0987123456");
-//    	user.setAddress("火星");
-//
-//
-//    	user.setCreateTime(nowTimeString);
-//    	user.setUpdateTime(nowTimeString);
-//
-//    	Integer rows = userMapper.insert(user);
-//    	System.out.println("新增幾筆資料: " + rows);
-//    }
-//}
+public class UserMapperTest {
+	@Autowired
+	private UserMapper userMapper;
+
+	@Test
+    public void insert() {
+		System.out.println("================");
+		System.out.println("測試使用者新增");
+
+		Date date = new Date();
+    	String nowTimeString = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date);
+
+    	String md5String = DigestUtils.md5DigestAsHex(nowTimeString.getBytes());
+		String testString = "test_" + md5String.substring(0, 3);;
+		System.out.println("testString: "+ testString);
+
+    	User user = new User();
+    	user.setUsername(testString);
+    	user.setPassword("123");
+    	user.setEmail(testString + "@gmail.com");
+    	user.setPhone("0987123456");
+
+    	Integer rows = userMapper.insert(user);
+    	System.out.println("新增幾筆資料: " + rows);
+    }
+
+    @Test
+	public void updatePasswordByUid(){
+		System.out.println("================");
+		System.out.println("測試使用者修改密碼");
+		userMapper.updatePasswordByUid(4, "00", "userMapperTest", new Date());
+	}
+
+	@Test
+	public void findByUid(){
+		System.out.println("================");
+		System.out.println("測試使用者取的單筆資料byUid");
+		System.out.println(userMapper.findByUid(4));
+	}
+}
